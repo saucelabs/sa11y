10b0966ad95d5d3120dee34c21601e623261426b
titusfortner <titusfortner@gmail.com>
2021-06-23 18:43:04 -0500

    [ruby] support analyzing within iframes

--- ruby/lib/sa11y/analyze.rb
+++ ruby/lib/sa11y/analyze.rb
@@ -5,7 +5,7 @@ require "selenium-webdriver"
 module Sa11y
   # Analyzes Current Webpage with axe™ Accessibility Tool from Deque
   class Analyze
-    attr_accessor :js_lib
+    attr_accessor :js_lib, :iframes
 
     # https://github.com/dequelabs/axe-core-maven-html/blob/61447b/src/main/java/com/deque/html/axecore/selenium/
     # AxeBuilder.java#L83-L95
@@ -20,11 +20,30 @@ module Sa11y
     def initialize(driver, js_lib: nil)
       @driver = driver
       @js_lib = js_lib || File.read(File.expand_path('../../scripts/axe.min.js', __FILE__))
+      @iframes = true
     end
 
     def results
       @driver.execute_script(@js_lib)
+      if iframes
+        @driver.switch_to.default_content
+        manage_frames
+      end
       @driver.execute_async_script(AXE_RESULTS, nil, "{}")
+    end
+
+    private
+
+    def manage_frames
+      frames = @driver.find_elements(tag_name: "iframe")
+      return if frames.empty?
+
+      frames.each do |frame|
+        @driver.switch_to.frame(frame)
+        @driver.execute_script(js_lib)
+        manage_frames
+        @driver.switch_to.parent_frame
+      end
     end
   end
 end
--- ruby/spec/analyze_spec.rb
+++ ruby/spec/analyze_spec.rb
@@ -19,5 +19,24 @@ module Sa11y
       results = analyze.results
       expect(results.dig("testEngine", "version")).to eq "3.5.3"
     end
+
+    it "handles iframes" do
+      @driver.get("http://watir.com/examples/nested_iframes.html")
+
+      expect_any_instance_of(Selenium::WebDriver::Driver)
+        .to receive(:switch_to).exactly(7).times.and_call_original
+
+      Analyze.new(@driver).results
+    end
+
+    it "ignores iframes when requested" do
+      analyze = Analyze.new(@driver)
+      analyze.iframes = false
+
+      @driver.get("http://watir.com/examples/iframes.html")
+
+      expect_any_instance_of(Selenium::WebDriver::Driver).not_to receive(:switch_to)
+      analyze.results
+    end
   end
 end
